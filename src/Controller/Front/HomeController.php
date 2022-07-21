f1b0a4f4304b260289250034b773b867a331f53b
N1Code7 <n1code7@outlook.fr>
2022-07-21 16:29:44 +0200

    Feature # Implementation of querybiulder for display last nth books on home page

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+    #[Route('/home/{number}', name: 'app_home')]
+    public function index(BookRepository $repository, int $number): Response
+    {
+        $books = $repository->findLastNTH($number);
+
+        return $this->render('home/index.html.twig', [
+            'books' => $books,
+            "number" => $number
+        ]);
+    }
+}
--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -39,28 +39,37 @@ class BookRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Book[] Returns an array of Book objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('b.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findLastNTH(int $number): array
+    {
+        return $this->createQueryBuilder("book")
+            ->orderBy("book.id", "DESC")
+            ->setMaxResults($number)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Book
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Book[] Returns an array of Book objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('b')
+    //            ->andWhere('b.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('b.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Book
+    //    {
+    //        return $this->createQueryBuilder('b')
+    //            ->andWhere('b.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
